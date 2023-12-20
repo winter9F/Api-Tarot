c6b9505cf714c3cf72ac7f5f571e39c2a0129afd
winter9F <147862480+winter9F@users.noreply.github.com>
2023-12-20 14:27:52 -0800

    Update 3

--- tarotCards.js
+++ tarotCards.js
@@ -1,11 +1,3 @@
-const app = require("express")();
-const PORT = 8111;
-
-
-app.listen(PORT, () => {
-    console.log("PORT 8111 Active!")
-})
-
 const tarotCards = [
     {
         card: "The Fool",
@@ -478,36 +470,4 @@ const tarotCards = [
 
 ];
 
-
-
-
-
-app.get("/tarot", (req, res) => {
-    let Past = 0;
-    let Present = 0;
-    let Future = 0;
-    let usedNums = [];
-    for (let i = 1; i < 4; i += 1) {
-
-        let index = Math.floor(Math.random() * 78);
-        if (usedNums.includes(index)) {
-            index = Math.floor(Math.random() * 78);
-        };
-        usedNums.push(index);
-        if (i <= 1) {
-            Past = tarotCards[index];
-        } else if (i <= 2) {
-            Present = tarotCards[index];
-        } else {
-            Future = tarotCards[index];
-        };
-    };
-    res.status(200).send({
-        Past,
-        Present,
-        Future,
-
-    });
-});
-
-
+module.exports = tarotCards
